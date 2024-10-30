f3a8073ecfe74323311ea4c8754a9ff3d8e535d7
Lovely Nofuente <lovely.nofuente@ballys.com>
2024-10-30 13:55:38 +0800

    Add mocha awesome reporter

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://datacom.com/nz/en/',
     watchForFileChanges: false,
+    screenshotsFolder: 'cypress/screenshots',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
